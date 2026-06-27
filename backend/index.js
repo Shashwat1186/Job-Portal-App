771b315d208600829791fd3679b746d499d7f2dd
Shashwat Salil Warune <shashwatwarune@gmail.com>
2026-06-27 15:59:35 +0530

    Revert "DB connected"
    This reverts commit 01639da3a4bbff51d5337573efbc7dd891b8e471.

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,7 @@
 import express from "express";
-import cookieParser from "cookie-parser";
-import cors from "cors";
 import dotenv from "dotenv";
-import connectDB from "./utils/db.js";
-import userRoute from "./routes/user.route.js";
-import companyRoute from "./routes/company.route.js";
-import jobRoute from "./routes/job.route.js";
-import applicationRoute from "./routes/application.route.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 dotenv.config({});
 
@@ -20,21 +15,18 @@ const corsOptions = {
     origin:'http://localhost:5173',
     credentials:true
 }
-
 app.use(cors(corsOptions));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
-
-// api's
-app.use("/api/v1/user", userRoute);
-app.use("/api/v1/company", companyRoute);
-app.use("/api/v1/job", jobRoute);
-app.use("/api/v1/application", applicationRoute);
-
-
+// simple test route
+app.get("/", (req, res) => {
+    return res.status(200).json({
+        message: "Hello from backend",
+        success: true
+    })
+});
 
 app.listen(PORT,()=>{
-    connectDB();
     console.log(`Server running at port ${PORT}`);
 })
